c1b806d61dea6d4f8ee2ba9dc187ef1fee600a5c
Rafael Goesmann <rgo51@uclive.ac.nz>
2019-09-22 10:34:11 +1200

    bluetooth connectivity is not working

---
+++ main/bluetooth.h
@@ -0,0 +1,2 @@
+void init_bluetooth();
+void send_bluetooth_reading(uint16_t reading);
--- main/main.c
+++ main/main.c
@@ -6,6 +6,7 @@
 #include "potentiometer.h"
 #include "uart_remote.h"
 #include "sdkconfig.h"
+#include "bluetooth.h"
 
 #define JOYSTICK_SAMPLE_PERIOD 20
 #define SEND_JOYSTICK_SAMPLE_PERIOD 250
@@ -21,14 +22,15 @@ void read_joystick(void* circ_buff) {
 
 void send_joystick_reading(void* circ_buff) {
     CircularBuffer* buffer = (CircularBuffer*) circ_buff;
-
+    // Wait a bit to make sure that bluetooth is configured
+    vTaskDelay(10000 / portTICK_PERIOD_MS);
     // Wait till the circular buffer is completely filled before reading values
     vTaskDelay(JOYSTICK_SAMPLE_PERIOD * BUFFER_LENGTH / portTICK_PERIOD_MS);
 
     while(1) {
         uint16_t circ_buff_mean = mean_circ_buff(buffer);
         printf("The mean is: %d\n", circ_buff_mean);
-        send_uart(circ_buff_mean);
+        send_bluetooth_reading(circ_buff_mean);
         vTaskDelay(SEND_JOYSTICK_SAMPLE_PERIOD / portTICK_PERIOD_MS); 
     }
 }
@@ -41,8 +43,10 @@ void app_main(void)
     init_uart_config();
     CircularBuffer buffer;
     init_circ_buff(&buffer);
-    xTaskCreate(read_joystick, "read_joystick", 8000, (void*) &buffer, 1, NULL);
-    xTaskCreate(send_joystick_reading, "sending_joystick_reading", 8000, &buffer, 1, NULL);
+    init_bluetooth();
+
+//    xTaskCreate(read_joystick, "read_joystick", 8000, (void*) &buffer, 1, NULL);
+//   xTaskCreate(send_joystick_reading, "sending_joystick_reading", 8000, &buffer, 1, NULL);
 }
 
 //idf.py -p /dev/cu.SLAB_USBtoUART flash
